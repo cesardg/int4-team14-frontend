60fa109f5a6fa7699e5e2484a39df361d3700511
Cesar De Greve <cesar.de.greve@student.howest.be>
2021-06-02 21:53:04 +0200

    vercel test

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,6 @@ export default function Home() {
       <main>
         <h1 className="title">The better team 14 groupchat - Next.js x Ably x Spongebob live Chat Demo</h1>
         <Chat/>
-        <Spongebob/>
       </main>
 
     </div>

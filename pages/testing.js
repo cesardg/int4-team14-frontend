903d212c52db186db20488692ccb6e98c8a5e52f
Cesar De Greve <cesar.de.greve@student.howest.be>
2021-06-03 16:38:01 +0200

    begin home page

---
+++ pages/testing.js
@@ -0,0 +1,53 @@
+import Head from 'next/head'
+import dynamic from 'next/dynamic'
+import styles from './../styles/Home.module.css'
+import GameBoard from '../components/GameBoard';
+import { useState, useEffect } from 'react';
+
+const Chat = dynamic(() => import('../components/Chat'), { ssr: false });
+
+export default function Testing() {
+
+  let arr = []
+  let tempField;
+  const [field, setField] = useState(1);
+  // If pressed key is our target key then set to true
+  const downHandler = ({key}) => {
+    arr.push(key);
+    const index =   arr.indexOf("K")
+    if (index != -1 && arr[index - 1] == "Q" && arr[index - 2] == "V" ){
+      tempField = arr[index + 1]
+      if (tempField){
+        setField(tempField)
+        arr = [];
+      } 
+    } 
+  }
+
+
+
+  // Add event listeners
+  useEffect(() => {
+    window.addEventListener('keydown', downHandler);
+    return () => {
+      window.removeEventListener('keydown', downHandler);
+    };
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Team 14 groupchat</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="title">Team 14 groupchat - Next.js x Ably x Arduino demo</h1>
+        <Chat/>
+        <GameBoard currentField={field}/>
+      </main>
+
+    </div>
+  )
+}
---
+++ pages/user.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import styles from './../styles/Home.module.css'
+
+export default function User() {
+
+
+  return (
+    <div>
+      <Head>
+        <title>Us3r vs H4ck3r</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.container}>
+        <h1 className="title">Us3r</h1>
+      </main>
+
+    </div>
+  )
+}

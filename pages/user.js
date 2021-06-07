ca70663dd68a841054fc3e903ab719fd7a766f7f
Lieselot Geirnaert <lieselot.geirnaert@gmail.com>
2021-06-07 11:31:19 +0200

    remove firstname

--- pages/user.js
+++ pages/user.js
@@ -15,40 +15,55 @@ export default function User() {
         <h1 className="title">Us3r</h1>
         <article>
           <h2>Form</h2>
-          <form className={styles.form} onSubmit={(e) => handleSubmit(e)} >
-
-            <label className={styles.label} >
-              Naam
-              <input className={styles.input} type="text" name="surname" required />
+          <form className={styles.form} onSubmit={(e) => handleSubmit(e)}>
+            <label className={styles.label}>
+              Gebruikersnaam
+              <input
+                className={styles.input}
+                type="text"
+                name="username"
+                required
+              />
             </label>
 
-            <label className={styles.label} >
-              Achternaam
-              <input className={styles.input} type="text" name="name" required />
+            <label className={styles.label}>
+              Emailadres
+              <input
+                className={styles.input}
+                type="email"
+                name="email"
+                required
+              />
             </label>
 
-            <label className={styles.label} >
-              Username
-              <input className={styles.input} type="text" name="username" required />
+            <label className={styles.label}>
+              Wachtwoord
+              <input
+                className={styles.input}
+                type="password"
+                name="password"
+                required
+              />
             </label>
 
-            <label className={styles.label} >
-              Passwoord
-              <input className={styles.input} type="text" name="password" required />
+            <label className={styles.label}>
+              Herhaal wachtwoord
+              <input
+                className={styles.input}
+                type="password"
+                name="reppassword"
+                required
+              />
             </label>
 
-            <label className={styles.label} >
-              Herhaal Paswoord
-              <input className={styles.input} type="password" name="reppassword" required />
-            </label>
-
-  
-        
-            <input className={styles.button} type="submit" value="Start game as a user" />
+            <input
+              className={styles.button}
+              type="submit"
+              value="Start game as a user"
+            />
           </form>
         </article>
       </main>
-
     </div>
-  )
+  );
 }

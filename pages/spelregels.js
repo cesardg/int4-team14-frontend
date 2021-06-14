7cd5a64c5a084cf22d8cab38991c6322b4e53f80
Cesar De Greve <cesar.de.greve@student.howest.be>
2021-06-14 20:16:10 +0200

    end styling spelregels

--- pages/spelregels.js
+++ pages/spelregels.js
@@ -47,17 +47,17 @@ const Rules = () => {
             <div className={styles.shortContainer}>
               <article className={styles.article}>
                 <h3 className={styles.subTitle}>Het spel in het kort</h3>
-                <p>Hack-Tic is een spel waarbij een hacker en een user-account het tegen elkaar opnemen in de strijd om de gegevens van een account. De beide spelers beginnen bij “START” en verplaatsen hun pion tijdens hun beurt zoveel vakjes over het speelbord als ze gegooid hebben met de dobbelsteen. 
+                <p>Hack-Tic is een spel waarbij een hacker en een gebruikers account het tegen elkaar opnemen in de strijd om de gegevens van een account. De beide spelers beginnen bij “START” en verplaatsen hun pion tijdens hun beurt zoveel vakjes over het speelbord als ze gegooid hebben met de dobbelsteen. 
                     Naar gelang het vakje waar de speler op belandt, zijn er verschillende acties die hem vooruit kunnen helpen tijdens het spel. Dit spel wordt zowel online als op een fysiek bord gespeeld.</p>
               </article>
               <div className={styles.articlePurposeContainer}>
                 <article className={styles.articlePurpose}>
-                  <h3 className={styles.subTitle}>Doel van de user</h3>
+                  <h3 className={styles.subTitle}>Doel gebruiker</h3>
                   <p>Zijn account zo goed mogelijk beveiligen zodat het voor de hacker “onkraakbaar” wordt.</p>
                 </article>
                 <article className={styles.articlePurpose}>
-                  <h3 className={styles.subTitle}>Doel van de hacker</h3>
-                  <p>Alle tekens in het wachtwoord van de user te weten komen.</p>
+                  <h3 className={styles.subTitle}>Doel hacker</h3>
+                  <p>Alle tekens in het wachtwoord van de gebruiker te weten komen.</p>
                 </article>
               </div>
             </div>
@@ -74,10 +74,10 @@ const Rules = () => {
                   Dit spel kan gespeeld worden met twee laptops of met een laptop en een tablet. Voor je het spel kan beginnen, moet het spelbord eerst correct aangesloten worden door de usb-tabel bij de laptop in te steken. 
                 </p>
                 <p className={styles.para}>
-                 Eén speler start een nieuw spel, deze speler krijgt als eerste de keuze welke rol hij wilt aannemen tijdens het spel : hacker of user?  Daarna ontvangt hij een code die de tegenspeler correct moet invoeren in het “neem deel aan een spel”-vak.  De tegenspeler krijgt automatisch de andere rol toegewezen. 
+                 Eén speler start een nieuw spel, deze speler krijgt als eerste de keuze welke rol hij wilt aannemen tijdens het spel : hacker of gebruiker?  Daarna ontvangt hij een code die de tegenspeler correct moet invoeren in het “neem deel aan een spel”-vak.  De tegenspeler krijgt automatisch de andere rol toegewezen. 
                 </p>
                 <p className={styles.para}>
-                 De user moet een account aanmaken met een gebruikersnaam, e-mailadres, profielfoto, sterk wachtwoord en enkele interesses. Maar ook de hacker krijgt de kans om een avatar te kiezen en een schuilnaam en e-mailadres in te voeren.
+                 De gebruiker moet een account aanmaken met een gebruikersnaam, e-mailadres, profielfoto, sterk wachtwoord en enkele interesses. Maar ook de hacker krijgt de kans om een avatar te kiezen en een schuilnaam en e-mailadres in te voeren.
                 </p>
                 <p className={styles.para}>
                 Wanneer alle gegevens correct zijn ingegeven, nemen beide pionnen plaats op het startvak. De computer beslist welke speler mag beginnen. Dit zal weergegeven worden op het scherm. 
@@ -97,7 +97,7 @@ const Rules = () => {
                <article className={styles.article}>
                 <h3 className={styles.subTitle}>Het spel</h3>
                 <p >
-                  Als u aan de beurt bent gooit u de dobbelsteen en verplaatst u uw pion het gegooide aantal vakjes. Zorg ervoor dat u enkel stevig doorklikt op het laatste vakje waar de pion moet blijven staan. Afhankelijk van het vakje waarop u terecht komt, heeft u verschillende positieve of negatieve acties. Deze zullen anders zijn wanneer u als hacker of als user speelt.
+                  Als u aan de beurt bent gooit u de dobbelsteen en verplaatst u uw pion het gegooide aantal vakjes. Zorg ervoor dat u enkel stevig doorklikt op het laatste vakje waar de pion moet blijven staan. Afhankelijk van het vakje waarop u terecht komt, heeft u verschillende positieve of negatieve acties. Deze zullen anders zijn wanneer u als hacker of als gebruiker speelt.
                 </p>
                
               </article>
@@ -108,7 +108,7 @@ const Rules = () => {
         </div>
         <div className={styles.hackerUserWrapper}>
           <div className={styles.userLayoutWrapper}>
-              <WindowLayout title="de user" bg="var(--brown)" border="var(--red)">
+              <WindowLayout title="de gebruiker" bg="var(--brown)" border="var(--red)">
                 <div className={styles.imageContainer}>
                 <Image
                 src={`/assets/img/userpics/pf2.svg`}
@@ -119,9 +119,10 @@ const Rules = () => {
                 </div>
                 <div className={styles.userContainer}>
                   <article className={styles.article}>
-                  <h3 className={styles.subTitle}>De user</h3>
+                  <h3 className={styles.subTitle}>De gebruiker</h3>
+                  <h4 className={styles.smallTitle}>actie-vak</h4>
                   <p >
-                    Als u aan de beurt bent gooit u de dobbelsteen en verplaatst u uw pion het gegooide aantal vakjes. Zorg ervoor dat u enkel stevig doorklikt op het laatste vakje waar de pion moet blijven staan. Afhankelijk van het vakje waarop u terecht komt, heeft u verschillende positieve of negatieve acties. Deze zullen anders zijn wanneer u als hacker of als user speelt.
+                   Wanneer u op een actievak terecht komt, springt  op uw scherm een venster open waarin u de keuze krijgt uit 6 verschillende acties. Deze acties kunt u gebruiken om uw account uit de klauwen van de hacker te houden. Deze 6 acties luiden als volgt: 
                   </p>
                   </article>
                 </div>
@@ -129,11 +130,20 @@ const Rules = () => {
             </div>
           <div className={styles.hackerLayoutWrapper}>
               <WindowLayout title="de hacker" bg="var(--black)" border="var(--green)">
+                 <div className={styles.imageContainer}>
+                <Image
+                src={`/assets/img/hackerpics/pf1.svg`}
+                alt="picture of user"
+                width={471}
+                height={302}
+                 />
+                </div>
                 <div className={styles.hackerContainer}>
                   <article className={styles.article}>
-                  <h3 className={styles.subTitle}>De user</h3>
+                  <h3 className={styles.subTitle}>De hacker</h3>
+                   <h4 className={styles.smallTitle}>actie-vak</h4>
                   <p >
-                    Als u aan de beurt bent gooit u de dobbelsteen en verplaatst u uw pion het gegooide aantal vakjes. Zorg ervoor dat u enkel stevig doorklikt op het laatste vakje waar de pion moet blijven staan. Afhankelijk van het vakje waarop u terecht komt, heeft u verschillende positieve of negatieve acties. Deze zullen anders zijn wanneer u als hacker of als user speelt.
+                   Wanneer u op een actievak terecht komt, springt  op uw scherm een venster open waarin u de keuze krijgt uit 6 verschillende acties om het account van de user binnen te dringen. Deze 6 acties luiden als volgt: 
                   </p>
                   </article>
                 </div>
